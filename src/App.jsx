10312b3f1d640ba09e8f657cdb0ca957ec1fa96e
amberlislaya <laya.playapps@gmail.com>
2023-07-31 20:13:58 -0300

    stream

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Landing from './pages/Landing/Landing'
+import Home from './pages/Home/Home'
+import Movies from './pages/Movies/Movies'
+import Series from './pages/Series/Series'
+
 
 const App = () => {
+
   return (
-	<div>App</div>
-  )
+
+<BrowserRouter>
+ <Routes>
+	<Route exact path='/' element={<Landing/>} />
+	<Route exact path='/home' element={<Home/>} />
+	<Route exact path='/movies' element={<Movies/>} />
+	<Route exact path='/series' element={<Series/>} />
+ </Routes>
+</BrowserRouter>
+)
 }
 
 export default App
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,18 +1,75 @@
-import React from 'react';
+import React from 'react'
+import './landing.css'
 
 
 const Landing = () => {
-  return (
-    <div>
-      <Header />
-      <main>
-        {/* Aquí puedes agregar contenido para la página de aterrizaje */}
-        <h1>Bienvenido a StreamMovie</h1>
-        <p>¡Disfruta de nuestras películas y series!</p>
-      </main>
-      {/* Aquí podrías agregar un CTA para redirigir a la página de inicio */}
-    </div>
-  );
-};
 
-export default Landing;
+  return (
+<div className='container'>
+  <div className='img-container'>
+  <img style={{ width: 2680, height: 1004, border: '0.50px black solid' }} src="series-netflix.jpg" />
+<div className='container-div1'>Disfruta de las mejores películas y series</div>
+<button className='button-container1'>Acceder</button>
+</div>
+
+<div className='container-div'>
+        <h1>Disfruta en tu TV<span><br />
+Ve en smart TV, Chromecast,
+Apple TV, reproductores  y más.</span></h1>
+<img src="" alt="" />
+</div>
+
+<div className='container-div'>
+  <h1>Descarga tus series para verlas forever.</h1>
+  <img src="" alt="" />
+</div>
+
+<div className='container-div'>
+  <h1>Disfruta donde quieras</h1>
+  <div className='img-container'>
+  <img src="" alt="" />
+  </div>
+</div>
+
+<div className='container-div'>
+  <h1>Descarga tus series para verlas forever</h1>
+  <h3>Ingresa tu email para crear una cuenta o reiniciar tu membresía</h3>
+  <input type="text" placeholder=''/>
+  <button className='button-container'> INGRESAR</button>
+  <img src="" alt="" />
+</div>
+
+<div className='container-div'>
+  <h1>Quienes Somos</h1>
+  <img src="" alt="" />
+  <img src="" alt="" />
+  <img src="" alt="" />
+  <img src="" alt="" />
+  <img src="" alt="" />
+  <img src="" alt="" />
+  <img src="" alt="" />
+  <img src="" alt="" />
+</div>
+
+
+<footer className='container-footer'>
+  <h1>¿Preguntas?,  envía un email a StreamPlay@gmail.com</h1>
+  <h3>Suscripciones</h3>
+  <h3>Privacidad</h3>
+  <h3>Centro de ayuda</h3>
+  <h3>Términos de uso</h3>
+  <h2>Contáctenos</h2>
+  <h4>© 2023 StreamPlay. Todos los derechos reservados.</h4>
+  <a href="instagram"></a>
+  <a href="gmail"></a>
+</footer>
+</div>
+
+
+
+ 
+
+)
+}
+
+export default Landing
