1ae7a54f1bfee384e808c2266775196b0876d4e1
amberlislaya <laya.playapps@gmail.com>
2023-08-02 05:24:00 -0300

    landing22

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,75 +1,128 @@
-import React from 'react'
-import img from '../../assets/series-netflix.jpg'
-import './landing.css'
+import React from 'react';
+import fondo from '../../assets/img.jpg';
+import logo from '../../assets/Logo.ico.png';
+import img1 from '../../assets/amber1.jpg'
+import img2 from '../../assets/nico.png'
+import img3 from '../../assets/luis.jpeg'
+import disfruta from '../../assets/peli2023.jpg'
+import descarga from '../../assets/840_560.jpg'
+import disfruta2 from '../../assets/tendencias.jpg'
+import '@fortawesome/fontawesome-free/css/all.css';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+// import { useState } from 'react';
+import style from './landing.module.css';
+
 
 
 const Landing = () => {
 
+  const navigate = useNavigate();
+  const clickHandler = () => {
+    navigate("/home");
+  };
+
   return (
-<div>
-      <div className='container'>
-<h1>Disfruta de las mejores películas y series</h1>
-<button className='button-container1'>Acceder</button>
-</div>
+    <div className={style.container}>
+      <div className={style.img}>
+        <div className={style.imgOverlay}>
+        <img src={fondo} alt="img.jpg" className={style.fondo}  />
+        <img src={logo} alt="Logo.ico" className={style.logo}/>
+        <h1 className={style.h1}>Disfruta de las mejores películas y series</h1>
+        </div>
+        <button onClick={clickHandler} className={style.boton}>Acceder</button>
+        <button className={`${style.registre} ${style.registreTopRight}`} >Registro</button>
+      </div>
 
-<div className='container-div'>
-        <h2>Disfruta en tu TV<span><br />
-Ve en smart TV, Chromecast,
-Apple TV, reproductores  y más.</span></h2>
-<img src="" alt="" />
-</div>
+      <div className={style.div1}>
+        <h2 className={`${style.title} ${style.titleLeft}`}>Disfruta en tu TV<span><br />
+          Ve en smart TV, Chromecast,
+          Apple TV, reproductores  y más.</span></h2>
+        <div className={style.tvContainer}>
+        <img src={disfruta} alt="peli2023.jpg" className={style.tvImage} />
+      </div>
+      </div>
 
-<div className='container-div'>
-  <h2>Descarga tus series para verlas forever.</h2>
-  <img src="" alt="" />
-</div>
+      <div className={style.div1}>
+        <h2 className={`${style.title} ${style.titleLeft}`}>Descarga tus series para verlas forever.</h2>
+        <div className={style.tvContainer}>
+        <img src={descarga} alt="840_560.jpg" className={style.tvImage} />
+      </div>
+      </div>
 
-<div className='container-div'>
-  <h2>Disfruta donde quieras</h2>
-  <div className='img-container'>
-  <img src="" alt="" />
-  </div>
-</div>
+      <div className={style.div1}>
+        <h2 className={`${style.title} ${style.titleLeft}`}>Disfruta donde quieras y como quieras</h2>
+        <div className={style.tvContainer}>
+        <img src={disfruta2} alt="tendencias.jpg" className={style.tvImage} />
+      </div>
+      </div>
 
-<div className='container-div'>
-  <h2>Descarga tus series para verlas forever</h2>
-  <h3>Ingresa tu email para crear una cuenta o reiniciar tu membresía</h3>
-  <input type="text" placeholder=''/>
-  <button className='button-container'> INGRESAR</button>
-  <img src="" alt="" />
-</div>
+      <div className={style.div3}>
+        <h2 className={style.title2}>Descarga tus series para verlas forever</h2>
+        <h4>Ingresa tu email para crear una cuenta o reiniciar tu membresía</h4>
+        <input className={style.input} type="text" placeholder='' />
+        <button className={style.input2}> INGRESAR</button>
+      </div>
 
-<div className='container-div'>
-  <h1>Quienes Somos</h1>
-  <img src="" alt="" />
-  <img src="" alt="" />
-  <img src="" alt="" />
-  <img src="" alt="" />
-  <img src="" alt="" />
-  <img src="" alt="" />
-  <img src="" alt="" />
-  <img src="" alt="" />
-</div>
+      <div className={style.div2}>
+        <h1 className={style.title1}>Quienes Somos</h1>
+        <div className={style.grid}>
+        <div className={style.circle}>
+        <img src={img1} alt="amber1.jpg" className={style.img1} />
+            <div className={style.textContainer}>
+              <p className={style.name}>Nombre 1</p>
+              <p className={style.career}>Carrera 1</p>
+            </div>
+        </div>
 
+        <div className={style.circle}>
+          <img src={img3} alt="luis.jpeg" className={style.img3} />
+        </div>
+       
+        <div className={style.circle}>
+          <img src={img2} alt="nico.png" className={style.img2} />
+        </div>
 
-<footer className='container-footer'>
-  <h1>¿Preguntas?,  envía un email a StreamPlay@gmail.com</h1>
-  <h3>Suscripciones</h3>
-  <h3>Privacidad</h3>
-  <h3>Centro de ayuda</h3>
-  <h3>Términos de uso</h3>
-  <h2>Contáctenos</h2>
-  <h4>© 2023 StreamPlay. Todos los derechos reservados.</h4>
-  <a href="instagram"></a>
-  <a href="gmail"></a>
-</footer>
-</div>
+        <div className={style.circle}>
+          <img src="" alt="" />
+        </div>
+
+        <div className={style.circle}>
+          <img src="" alt="" />
+        </div>
+
+        <div className={style.circle}>
+          <img src="" alt="" />
+        </div>
+
+        <div className={style.circle}>
+          <img src="" alt="" />
+        </div>
+
+        <div className={style.circle}>
+          <img src="" alt="" />
+        </div>
+      </div>
+      </div>
+
+      <footer className={style.footer}>
+        <h1 className={style.titlefo}>¿Preguntas?,  envía un email a moviesplay@gmail.com</h1>
+        <Link to="/suscripciones">Suscripciones</Link>
+        <Link to="/privacidad">Privacidad</Link>
+        <Link to="/centrodeayuda">Centro de ayuda</Link>
+        <Link to="/terminosdeuso">Términos de uso</Link>
+        <h4>Contáctenos</h4>
+        <a href="https://www.instagram.com/" target="_blank"><i className="fab fa-instagram icono-red"></i></a>
+        <h6>© 2023 MoviePlay. Todos los derechos reservados.</h6>
+      </footer>
+    </div>
+  );
+};
+
+export default Landing;
+
 
 
 
  
 
-)
-}
-
-export default Landing
