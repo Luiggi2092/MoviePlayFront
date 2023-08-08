7173aa82cc8b1d14494480f09dcaaee95cd826fb
e-commerceTechComputers <85269071+e-commerceTechComputers@users.noreply.github.com>
2023-08-07 22:12:53 -0500

    deployok

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,14 @@
 import { createStore,applyMiddleware,compose} from "redux";
 import rootReducer from "./reducer";
 import thunkMiddleware from "redux-thunk";
-import {composeWithDevTools} from "redux-devtools-extension"
+//import {composeWithDevTools} from "redux-devtools-extension"
 
 
-const composeEnhander = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+const composeEnhander =  compose;
 
 const store = createStore(
     rootReducer,
-    composeEnhander(applyMiddleware(thunkMiddleware),composeWithDevTools())
+    composeEnhander(applyMiddleware(thunkMiddleware))
 );
 
 
