867dcca06f53c737a5d08b4c95d570a70db3d31d
MICastilloHurtado <122422130+MICastilloHurtado@users.noreply.github.com>
2023-08-13 00:07:21 -0500

    actualizacion

--- src/components/CardCar/CardCar.jsx
+++ src/components/CardCar/CardCar.jsx
@@ -1,13 +1,33 @@
 import style from './CardCar.module.css'
+import { removeFromCartAndRemoveDetailsSerie, removeFromCartAndRemoveDetailsMovie} from '../../redux/actions'
+import { useDispatch } from 'react-redux'
 
 const CardCar = (props) => {
+
+    const dispatch = useDispatch()
+
+    const handleClick = () => {
+        if(props.tipo === 'serie'){
+            dispatch(removeFromCartAndRemoveDetailsSerie(props.id))            
+        }
+        if(props.tipo === 'movie'){
+            dispatch(removeFromCartAndRemoveDetailsMovie(props.id))
+        }
+
+    }
+
     return(
         <div className={style.container}>
             <img src={props.image} className={style.image} />
-            <h4>{props.name}</h4>
-            <h4>${props.price}</h4>
-            <span>item #{props.id}</span>
-            <button>Eliminar</button>
+            <div className={style.containerName}>
+            <h4 className={style.name}>{props.name}</h4>
+            </div>
+            <div className={style.priceContainer}>
+            <h4 className={style.price}>${props.price}</h4>
+            </div>
+            <div className={style.containerBoton}>
+            <button className={style.boton} onClick={handleClick}>Eliminar</button>
+            </div>
         </div>
     )
 }
